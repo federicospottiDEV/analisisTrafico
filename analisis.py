9967cb9616f3d51fd1a24d32121e763334145e9f
federicospottiDEV <federicotiver@gmail.com>
2025-05-09 02:17:13 -0300

    v2

--- analisis.py
+++ analisis.py
@@ -1,63 +1,66 @@
 from scapy.all import sniff, IP
 
-MAX_PAQUETES = 2000
+MAX_PAQUETES = 100
 
-# Diccionario para traducir el número de protocolo a nombre
+
 PROTOCOLOS = {
-    1: "ICMP",
-    6: "TCP",
-    17: "UDP",
-    47: "GRE",
-    50: "ESP"
+ 1: "ICMP",
+ 6: "TCP",
+ 17: "UDP",
+ 47: "GRE",
+ 50: "ESP"
 }
 
 ipTraficoEntrada = {}
-ipTraficoSalida = {}
-
-# Contador de protocolos
+ipTraficoDestino = {}
 conteo_protocolo = {}
+
+
 
 def mostrar_paquete(packet):
     if packet.haslayer(IP):
         capa_ip = packet[IP]
+
         protocolo_num = capa_ip.proto
         protocolo_nombre = PROTOCOLOS.get(protocolo_num, f"Otro ({protocolo_num})")
+
         ipEntrada= capa_ip.src 
+        ipDestino= capa_ip.dst
 
-        # Contar protocolo
-        if protocolo_nombre in conteo_protocolo:
-            conteo_protocolo[protocolo_nombre] += 1
-        else:
-            conteo_protocolo[protocolo_nombre] = 1
+        sumarContador(protocolo_nombre, conteo_protocolo)
+        sumarContador(ipEntrada, ipTraficoEntrada)
+        sumarContador(ipDestino, ipTraficoDestino)
 
-
-        # Contar ip entrada
-        if ipEntrada in ipTraficoEntrada:
-            ipTraficoEntrada[ipEntrada] += 1
-        else:
-            ipTraficoEntrada[ipEntrada] = 1
-
-
-
-       # print(f"De: {capa_ip.src} -> A: {capa_ip.dst} | Protocolo: {protocolo_nombre} | Tamaño: {len(packet)} bytes")
+       # en caso de querer ver los paquetes descomentar print(f"De: {capa_ip.src} -> A: {capa_ip.dst} | Protocolo: {protocolo_nombre} | Tamaño: {len(packet)} bytes")
     else:
         print("Paquete sin capa IP")
 
+def sumarContador(a, b):
+    if a in b:
+        b[a] += 1
+    else:
+        b[a] = 1
+    
 
-def obtenerCantidad(par):
-    return par[1]
-
-# Captura de paquetes
 sniff(filter="ip", prn=mostrar_paquete, count=MAX_PAQUETES)
 
-# Mostrar resumen
+
 print("\nResumen de protocolos:")
 for protocolo, cantidad in conteo_protocolo.items():
     print(f"{protocolo}: {cantidad} paquetes")
 
+def obtenerCantidad(par):
+    return par[1]
 
 ordenadoEntrada = sorted(ipTraficoEntrada.items(), key = obtenerCantidad, reverse= True) 
+ordenadoDestino= sorted(ipTraficoDestino.items(), key = obtenerCantidad, reverse= True) 
 
-print("\n:")
-for i in range(5):
+
+# aca printeo las 5 principales ip entrada/destino
+print("\n5 direcciones IP de origen con mayor tráfico")
+for i in range(min(5, len(ordenadoEntrada))):
     print(f"{ordenadoEntrada[i][0]}: {ordenadoEntrada[i][1]} paquetes")
+print("\n5 direcciones IP de destino con mayor tráfico")
+for i in range(min(5, len(ordenadoDestino))):
+    print(f"{ordenadoDestino[i][0]}: {ordenadoDestino[i][1]} paquetes")
+
